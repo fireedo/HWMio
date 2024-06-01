124b09f19babbc28a1f6f77a39e22a9a54b45494
fireedo <51096156+fireedo@users.noreply.github.com>
2024-06-01 18:54:46 +0700

    Update GpuOverclockWidget.cpp

--- GpuOverclockWidget.cpp
+++ GpuOverclockWidget.cpp
@@ -14,6 +14,14 @@ GpuOverclockWidget::GpuOverclockWidget(QWidget *parent) : QWidget(parent) {
     memoryClockOffset->setRange(-1000, 1000); // Example range, adjust as needed
     memoryClockOffset->setSuffix(" MHz");
 
+    fanSpeed = new QSpinBox();
+    fanSpeed->setRange(-1, 100); // -1 for default, 0-100 for fan speed percentage
+    fanSpeed->setSuffix(" %");
+
+    powerLimit = new QSpinBox();
+    powerLimit->setRange(-1, 300); // -1 for default, 0-300 for power limit in watts
+    powerLimit->setSuffix(" W");
+
     resultLabel = new QLineEdit();
     resultLabel->setReadOnly(true);
 
@@ -22,6 +30,8 @@ GpuOverclockWidget::GpuOverclockWidget(QWidget *parent) : QWidget(parent) {
 
     formLayout->addRow("Core Clock Offset:", coreClockOffset);
     formLayout->addRow("Memory Clock Offset:", memoryClockOffset);
+    formLayout->addRow("Fan Speed:", fanSpeed);
+    formLayout->addRow("Power Limit:", powerLimit);
     formLayout->addRow("Result:", resultLabel);
     mainLayout->addLayout(formLayout);
     mainLayout->addWidget(applyButton);
@@ -36,11 +46,14 @@ GpuOverclockWidget::GpuOverclockWidget(QWidget *parent) : QWidget(parent) {
 void GpuOverclockWidget::applyOverclock() {
     int coreOffset = coreClockOffset->value();
     int memOffset = memoryClockOffset->value();
+    int fanSpd = fanSpeed->value();
+    int pwrLimit = powerLimit->value();
 
     QProcess process;
     QString program = "python3";
     QStringList arguments;
-    arguments << "gpu_overclock.py" << "0" << QString::number(coreOffset) << QString::number(memOffset);
+    arguments << "gpu_overclock.py" << "0" << QString::number(coreOffset) << QString::number(memOffset)
+    << QString::number(fanSpd) << QString::number(pwrLimit);
 
     process.start(program, arguments);
     process.waitForFinished();
@@ -52,11 +65,14 @@ void GpuOverclockWidget::applyOverclock() {
 void GpuOverclockWidget::setToDefault() {
     int coreOffset = 0;
     int memOffset = 0;
+    int fanSpd = -1; // Default fan speed
+    int pwrLimit = -1; // Default power limit
 
     QProcess process;
     QString program = "python3";
     QStringList arguments;
-    arguments << "gpu_overclock.py" << "0" << QString::number(coreOffset) << QString::number(memOffset);
+    arguments << "gpu_overclock.py" << "0" << QString::number(coreOffset) << QString::number(memOffset)
+    << QString::number(fanSpd) << QString::number(pwrLimit);
 
     process.start(program, arguments);
     process.waitForFinished();
@@ -66,4 +82,6 @@ void GpuOverclockWidget::setToDefault() {
 
     coreClockOffset->setValue(coreOffset);
     memoryClockOffset->setValue(memOffset);
+    fanSpeed->setValue(fanSpd);
+    powerLimit->setValue(pwrLimit);
 }
