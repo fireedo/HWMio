cbf4f2252177b3ea72a87ef2f6253f2acf60a826
fireedo <51096156+fireedo@users.noreply.github.com>
2024-06-01 18:56:32 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,13 @@
-#include <QApplication>
+#include <QtCore/qglobal.h>
+
+#if QT_VERSION_MAJOR >= 6
+#include <QtWidgets/QApplication>
+#elif QT_VERSION_MAJOR >= 5
+#include <QtWidgets/QApplication>
+#else
+#error Unsupported Qt version
+#endif
+
 #include "MainWindow.h"
 
 int main(int argc, char *argv[]) {
