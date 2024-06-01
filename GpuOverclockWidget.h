f9a3e70186dafe2e51646ac549dec0464fde6821
fireedo <51096156+fireedo@users.noreply.github.com>
2024-06-01 18:55:05 +0700

    Update GpuOverclockWidget.h

--- GpuOverclockWidget.h
+++ GpuOverclockWidget.h
@@ -1,12 +1,25 @@
 #ifndef GPUOVERCLOCKWIDGET_H
 #define GPUOVERCLOCKWIDGET_H
 
-#include <QWidget>
-#include <QLineEdit>
-#include <QSpinBox>
-#include <QPushButton>
-#include <QVBoxLayout>
-#include <QFormLayout>
+#include <QtCore/qglobal.h>
+
+#if QT_VERSION_MAJOR >= 6
+#include <QtWidgets/QWidget>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QSpinBox>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QFormLayout>
+#elif QT_VERSION_MAJOR >= 5
+#include <QtWidgets/QWidget>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QSpinBox>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QFormLayout>
+#else
+#error Unsupported Qt version
+#endif
 
 class GpuOverclockWidget : public QWidget {
     Q_OBJECT
@@ -20,6 +33,8 @@ private slots:
 private:
     QSpinBox *coreClockOffset;
     QSpinBox *memoryClockOffset;
+    QSpinBox *fanSpeed;
+    QSpinBox *powerLimit;
     QLineEdit *resultLabel;
     QPushButton *applyButton;
     QPushButton *defaultButton;
