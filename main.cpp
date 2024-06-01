a8d22882166dc64413313f0ff3c108fc8e25cc70
fireedo <51096156+fireedo@users.noreply.github.com>
2024-06-01 14:13:28 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,11 @@
-#include <QtWidgets>
-#include "SystemInfoWidget.h"
-#include "CpuTemperatureWidget.h"
-#include "GpuInfoWidget.h"
-#include "GpuOverclockWidget.h"
+#include <QApplication>
+#include "MainWindow.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
-    QWidget window;
-    QVBoxLayout *layout = new QVBoxLayout(&window);
-
-    SystemInfoWidget *systemInfo = new SystemInfoWidget();
-    CpuTemperatureWidget *cpuTemp = new CpuTemperatureWidget();
-    GpuInfoWidget *gpuInfo = new GpuInfoWidget();
-    GpuOverclockWidget *gpuOverclock = new GpuOverclockWidget();
-
-    layout->addWidget(systemInfo);
-    layout->addWidget(cpuTemp);
-    layout->addWidget(gpuInfo);
-    layout->addWidget(gpuOverclock);
-
-    window.setLayout(layout);
-    window.show();
+    MainWindow mainWindow;
+    mainWindow.show();
 
     return app.exec();
 }
