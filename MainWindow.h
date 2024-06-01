c8075e138d730fe2866a2d6e17b1b6adeff523a1
fireedo <51096156+fireedo@users.noreply.github.com>
2024-06-01 18:55:53 +0700

    Update MainWindow.h

--- MainWindow.h
+++ MainWindow.h
@@ -1,16 +1,26 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
-#include <QToolBox>
+#include <QtCore/qglobal.h>
 
-class SystemInfoWidget;
-class CpuTemperatureWidget;
-class GpuInfoWidget;
-class GpuOverclockWidget;
+#if QT_VERSION_MAJOR >= 6
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QToolBox>
+#include <QtWidgets/QPushButton>
+#elif QT_VERSION_MAJOR >= 5
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QToolBox>
+#include <QtWidgets/QPushButton>
+#else
+#error Unsupported Qt version
+#endif
 
-class MainWindow : public QMainWindow
-{
+#include "SystemInfoWidget.h"
+#include "CpuTemperatureWidget.h"
+#include "GpuInfoWidget.h"
+#include "GpuOverclockWidget.h"
+
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
@@ -27,6 +37,8 @@ private:
     CpuTemperatureWidget *cpuTemperatureWidget;
     GpuInfoWidget *gpuInfoWidget;
     GpuOverclockWidget *gpuOverclockWidget;
+    QPushButton *expandAllButton;
+    QPushButton *collapseAllButton;
 };
 
 #endif // MAINWINDOW_H
