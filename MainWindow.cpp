789b67a6fe7ae063e76d0bb9e120e8b32ec55c8a
fireedo <51096156+fireedo@users.noreply.github.com>
2024-06-01 18:56:12 +0700

    Update MainWindow.cpp

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,61 +1,42 @@
 #include "MainWindow.h"
-#include "SystemInfoWidget.h"
-#include "CpuTemperatureWidget.h"
-#include "GpuInfoWidget.h"
-#include "GpuOverclockWidget.h"
-#include <QVBoxLayout>
-#include <QPushButton>
-#include <QToolBox>
+
+#if QT_VERSION_MAJOR >= 6
+#include <QtWidgets/QToolBox>
+#include <QtWidgets/QPushButton>
+#elif QT_VERSION_MAJOR >= 5
+#include <QtWidgets/QToolBox>
+#include <QtWidgets/QPushButton>
+#else
+#error Unsupported Qt version
+#endif
 
 MainWindow::MainWindow(QWidget *parent)
-: QMainWindow(parent), toolBox(new QToolBox(this))
-{
-    QWidget *centralWidget = new QWidget(this);
-    QVBoxLayout *layout = new QVBoxLayout(centralWidget);
-
+: QMainWindow(parent), toolBox(new QToolBox(this)) {
     systemInfoWidget = new SystemInfoWidget(this);
     cpuTemperatureWidget = new CpuTemperatureWidget(this);
     gpuInfoWidget = new GpuInfoWidget(this);
     gpuOverclockWidget = new GpuOverclockWidget(this);
 
     toolBox->addItem(systemInfoWidget, "Informasi CPU");
-    toolBox->addItem(cpuTemperatureWidget, "CPU Temperature Information");
-    toolBox->addItem(gpuInfoWidget, "GPU Information");
-    toolBox->addItem(gpuOverclockWidget, "Overclock Function");
+    toolBox->addItem(cpuTemperatureWidget, "Temperatur CPU");
+    toolBox->addItem(gpuInfoWidget, "Informasi GPU");
+    toolBox->addItem(gpuOverclockWidget, "Overclock GPU");
 
-    QPushButton *expandAllButton = new QPushButton("Expand All Sections", this);
-    QPushButton *collapseAllButton = new QPushButton("Collapse All Sections", this);
+    expandAllButton = new QPushButton("Expand All", this);
+    collapseAllButton = new QPushButton("Collapse All", this);
 
     connect(expandAllButton, &QPushButton::clicked, this, &MainWindow::expandAllSections);
     connect(collapseAllButton, &QPushButton::clicked, this, &MainWindow::collapseAllSections);
 
-    layout->addWidget(toolBox);
-    layout->addWidget(expandAllButton);
-    layout->addWidget(collapseAllButton);
-
-    setCentralWidget(centralWidget);
+    setCentralWidget(toolBox);
 }
 
-void MainWindow::expandAllSections()
-{
-    for (int i = 0; i < toolBox->count(); ++i)
-    {
-        toolBox->setCurrentIndex(i);
-        toolBox->widget(i)->show();
-    }
+MainWindow::~MainWindow() {}
+
+void MainWindow::expandAllSections() {
+    // Implement the functionality to expand all sections
 }
 
-void MainWindow::collapseAllSections()
-{
-    for (int i = 0; i < toolBox->count(); ++i)
-    {
-        toolBox->widget(i)->hide();
-    }
-    toolBox->setCurrentIndex(0);
-    toolBox->widget(0)->show();
-}
-
-MainWindow::~MainWindow()
-{
-    // Qt will automatically delete child widgets when the parent is deleted
+void MainWindow::collapseAllSections() {
+    // Implement the functionality to collapse all sections
 }
